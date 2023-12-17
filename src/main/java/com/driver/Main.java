9f16f28ee49849c3d4c38c398e418423036e00be
s-ourav <sourav.mozu@gmail.com>
2023-12-17 14:07:34 +0530

    3 funcs

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
-public class Main {
+import java.sql.Struct;
 
+public class Main {
+public static void Main (String[] args){
+    Product p=new Product();
+    int ans=p.product(3,2);
+    int ans2=p.product(4,6,9);
+    double ans3=p.product(4.566,7.889);
+
+}
+public static class Product{
+    public int product(int x, int y) {
+        return x+y;
+    }
+
+    public int product(int x, int y, int z) {
+        return x+y*z;
+    }
+
+    public double product(double x, double y) {
+        return x+y;
+    }
+
+}
 }
